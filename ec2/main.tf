a24508801d861a96cddfe3bd1909eea530810a53
VitoCarlione <chaplygim@gmail.com>
2023-02-02 08:54:57 -0500

    add new config apache2

--- ec2/main.tf
+++ ec2/main.tf
@@ -26,9 +26,25 @@ resource "aws_instance" "web_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name = "id_rsa.pub"
+  user_data = <<EOF
+		      #! /bin/bash
+          sudo apt-get update
+		      sudo apt-get install -y apache2
+		      sudo systemctl start apache2
+		      sudo systemctl enable apache2
+		      echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+	EOF
 }
 
 resource "aws_key_pair" "webserver-key" {
     key_name = "id_rsa.pub"
     public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_eip" "eip" {
+  vpc      = true
+  instance = aws_instance.web_server.id
+  provisioner "local-exec" {
+    command = "echo ${aws_eip.eip.public_dns} >> /home/kl/terraform/ec2/web_server_public_dns.txt"
+  }
 }
